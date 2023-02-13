83973693be531f6e47eb5b50f52f4bf05e93bc64
bluuesz <wosleyv@protonmail.com>
2023-02-12 23:12:10 -0300

    add steam bot

---
+++ src/server/bot/steam/index.ts
@@ -0,0 +1,82 @@
+import TradeOfferManager from "steam-tradeoffer-manager";
+import SteamUser from "steam-user";
+import SteamTotp from "steam-totp";
+import SteamCommunity from "steamcommunity";
+import { env } from "../../../env/server.mjs";
+import { getBaseUrl } from "../../../utils/api.js";
+
+export const steam = () => {
+  const client = new SteamUser();
+  const community = new SteamCommunity();
+  const manager = new TradeOfferManager({
+    steam: client,
+    domain: getBaseUrl(),
+    language: "en",
+  });
+
+  const login = () =>
+    client.logOn({
+      accountName: env.STEAM_ACCOUNT_NAME,
+      password: env.STEAM_PASSWORD,
+      twoFactorCode: SteamTotp.generateAuthCode(env.STEAM_SHARED_SECRET),
+      machineName: env.STEAM_MACHINE_NAME || "localhost",
+    });
+
+  const onWebSession = (_: string, cookies: string[]) => {
+    manager.setCookies(cookies);
+    community.setCookies(cookies);
+  };
+
+  const acceptOffer = (offerId: string): Promise<string> =>
+    new Promise((resolve, reject) =>
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      community.acceptConfirmationForObject(
+        env.STEAM_IDENTITY_SECRET,
+        offerId,
+        (err) => {
+          if (err) {
+            reject(err);
+          }
+
+          resolve(offerId);
+        }
+      )
+    );
+
+  const createOffer = async (
+    tradeOfferUrl: string,
+    item: TradeOfferManager.EconItem
+  ) => {
+    const offer = manager.createOffer(tradeOfferUrl);
+    offer.addTheirItem(item);
+    offer.setMessage("Anyon -> Accept the trade to wrap this skin into NFT");
+
+    const offerSentStatus: "pending" | "sent" = await new Promise(
+      (resolve, reject) => {
+        offer.send((err, status) => {
+          if (err) reject(err);
+
+          resolve(status);
+        });
+      }
+    );
+
+    if (offerSentStatus === "pending") {
+      await acceptOffer(offer.id);
+    }
+
+    console.log(`Offer #${offer.id} sent successfully`);
+  };
+
+  const init = () => {
+    login();
+    client.on("loggedOn", () => console.log("Logged into Steam"));
+    client.on("webSession", onWebSession);
+
+    return {
+      createOffer,
+    };
+  };
+
+  return init();
+};
