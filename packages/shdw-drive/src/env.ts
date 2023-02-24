846f738b38452408ac2189c8d5f3f9c58086a24f
bluuesz <wosleyv@protonmail.com>
2023-02-24 20:42:42 -0300

    add shdw-drive package

---
+++ packages/shdw-drive/src/env.ts
@@ -0,0 +1,3 @@
+import { getEnv } from "@anyon/env";
+
+export const env = getEnv(["SHDW_DRIVE_AUTHORITY"]);
---
+++ packages/shdw-drive/src/index.ts
@@ -0,0 +1,31 @@
+import { ShadowFile, ShdwDrive } from "@shadow-drive/sdk";
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { env } from "./env";
+
+export const shdwDrive = async () => {
+  const connection = new Connection("mainnet");
+
+  const kp = Keypair.fromSecretKey(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Uint8Array.from(JSON.parse(env.SHDW_DRIVE_AUTHORITY) as any[])
+  );
+
+  const drive = await new ShdwDrive(connection, kp).init();
+
+  const createStorage = async (name: string, size?: string) => {
+    const storage = await drive.createStorageAccount(name, size || "1GB", "v2");
+
+    return storage;
+  };
+
+  const uploadFile = async (storagePubkey: PublicKey, file: ShadowFile) => {
+    const upload = await drive.uploadFile(storagePubkey, file);
+
+    return upload;
+  };
+
+  return {
+    createStorage,
+    uploadFile,
+  };
+};
