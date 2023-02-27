dfbfcb312b983d6de745e4c8601dbdf87f5466d3
bluuesz <wosleyv@protonmail.com>
2023-02-27 18:17:08 -0300

    add anchor wallet to instantiate shdwdrive

--- packages/shdw-drive/src/index.ts
+++ packages/shdw-drive/src/index.ts
@@ -1,16 +1,22 @@
+import * as anchor from "@project-serum/anchor";
 import { ShadowFile, ShdwDrive } from "@shadow-drive/sdk";
 import { Connection, Keypair, PublicKey } from "@solana/web3.js";
 import { env } from "./env";
 
 export const shdwDrive = async () => {
-  const connection = new Connection("mainnet");
+  // TODO: move to .env
+  const connection = new Connection(
+    "https://rpc.helius.xyz/?api-key=3c8d51cb-460e-459b-929d-edfccc126099"
+  );
 
   const kp = Keypair.fromSecretKey(
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     Uint8Array.from(JSON.parse(env.SHDW_DRIVE_AUTHORITY) as any[])
   );
 
-  const drive = await new ShdwDrive(connection, kp).init();
+  const wallet = new anchor.Wallet(kp);
+
+  const drive = await new ShdwDrive(connection, wallet).init();
 
   const createStorage = async (name: string, size?: string) => {
     const storage = await drive.createStorageAccount(name, size || "1GB", "v2");
