e9682df967a46aaa664773fe34e40c23596ce5c8
wosleyv <wosleyv@protonmail.com>
2023-03-24 19:46:33 -0300

    fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ const config = {
           "./tsconfig.json",
           "./apps/*/tsconfig.json",
           "./packages/*/tsconfig.json",
-          "./workers/tsconfig.json",
+          "./workers/*/tsconfig.json",
         ],
       },
       rules: {
